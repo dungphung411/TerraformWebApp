6d06145eeb9e48c1e381b9d15333096f66e0677e
dungphung411 <mdungphung411@gmail.com>
2024-07-15 16:50:18 +0700

    oke

--- vpc.tf
+++ vpc.tf
@@ -92,6 +92,20 @@ resource "aws_security_group" "ssh-security-group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+  engress {
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "SSH Security Group"
   }
